410d46952ef51e499ba5223d3e262fe8d5919239
Matheus Mattos <matheusmattos.r@gmail.com>
2025-07-13 21:26:30 -0300

    feat: Sessão Resume completa - Prenchimento das áreas skill e education - Adição de animação fluida na parte das skills

--- Assets/Js/resume.js
+++ Assets/Js/resume.js
@@ -1,3 +1,5 @@
+/*Carrosel*/
+
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     items: 1, //garante apenas 1 slide
@@ -15,4 +17,41 @@ $(document).ready(function () {
       1024: { items: 1 },
     },
   });
+});
+
+/*show and hide skills section*/
+
+const btncollapse1 = document.getElementById("btn-collapse1");
+const divcollapse1 = document.getElementById("collapse1");
+const btncollapse2 = document.getElementById("btn-collapse2");
+const divcollapse2 = document.getElementById("collapse2");
+
+btncollapse1.addEventListener("click", function () {
+  if (divcollapse1.classList.contains("hidden")) {
+    divcollapse1.classList.remove("hidden");
+    divcollapse1.classList.add("show");
+  } else {
+    divcollapse1.classList.remove("show");
+    divcollapse1.classList.add("hidden");
+  }
+
+  if (divcollapse2.classList.contains("show")) {
+    divcollapse2.classList.remove("show");
+    divcollapse2.classList.add("hidden");
+  }
+});
+
+btncollapse2.addEventListener("click", function () {
+  if (divcollapse2.classList.contains("hidden")) {
+    divcollapse2.classList.remove("hidden");
+    divcollapse2.classList.add("show");
+  } else {
+    divcollapse2.classList.remove("show");
+    divcollapse2.classList.add("hidden");
+  }
+
+  if (divcollapse1.classList.contains("show")) {
+    divcollapse1.classList.remove("show");
+    divcollapse1.classList.add("hidden");
+  }
 });
